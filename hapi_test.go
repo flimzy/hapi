bbed5218bad14c0aebdc8947812a61e6c201d3c2
Jonathan Hall <jhall@efolder.net>
2015-07-04 16:14:51 -0500

    Add test names

--- hapi_test.go
+++ hapi_test.go
@@ -31,14 +31,16 @@ func TestNew(t *testing.T) {
 }
 
 type typeTest struct{
+    Name        string
     Accept      string
     Handlers    map[string]Handle   
     Result      string
     HandlerID   string
 }
 
-func NewTest(accept,result,id string) *typeTest {
+func NewTest(name,accept,result,id string) *typeTest {
     return &typeTest{
+        name,
         accept,
         make(map[string]Handle),
         result,
@@ -53,7 +55,7 @@ func (testData *typeTest) AddHandler(ctype,id string) {
 func (testData *typeTest) DoTests(t *testing.T) {
     negType,negHandler := TypeNegotiator(testData.Accept,testData.Handlers)
     if negType != testData.Result {
-        t.Fatalf("TypeNegotiator returned '%s', expected '%s'\n", negType, testData.Result)
+        t.Fatalf("%s: TypeNegotiator returned '%s', expected '%s'\n", testData.Name, negType, testData.Result)
     }
     context := &Context{}
     context.Stash = make(map[string]interface{})
@@ -65,39 +67,39 @@ func (testData *typeTest) DoTests(t *testing.T) {
         id = context.Stash["id"].(string)
     }
     if id != testData.HandlerID {
-        t.Fatalf("Handler identified itself as '%s', expected '%s'\n", id, testData.HandlerID)
+        t.Fatalf("%s: Handler identified itself as '%s', expected '%s'\n", testData.Name, id, testData.HandlerID)
     }
 }
 
 // Tests 'text/html' handler
 func TestTypeNegotiator(t *testing.T) {
     // Accept: matches exactly
-    testData := NewTest("text/html","text/html","1")
+    testData := NewTest("test/html 1","text/html","text/html","1")
     testData.AddHandler("text/html","1")
     testData.DoTests(t)
 
     // Accept: Doesn't match
-    testData = NewTest("text/plain","","")
+    testData = NewTest("test/html 2","text/plain","","")
     testData.AddHandler("text/html","1")
     testData.DoTests(t)
 
     // Accept: doesn't exist
-    testData = NewTest("","text/html","1")
+    testData = NewTest("test/html 3","","text/html","1")
     testData.AddHandler("text/html","1")
     testData.DoTests(t)
 
     // Accept: */*
-    testData = NewTest("*/*","text/html","1")
+    testData = NewTest("test/html 4","*/*","text/html","1")
     testData.AddHandler("text/html","1")
     testData.DoTests(t)
 
     // Accept: text/*
-    testData = NewTest("text/*","text/html","1")
+    testData = NewTest("test/html 5","text/*","text/html","1")
     testData.AddHandler("text/html","1")
     testData.DoTests(t)
 
     // Accept: foo/*
-    testData = NewTest("text/plain","","")
+    testData = NewTest("test/html 6","text/plain","","")
     testData.AddHandler("text/html","1")
     testData.DoTests(t)
 }
@@ -105,32 +107,32 @@ func TestTypeNegotiator(t *testing.T) {
 // Tests for '*/*' handler
 func TestTypeNegotiator2(t *testing.T) {
     // Accept: text/html
-    testData := NewTest("text/html","text/html","1")
+    testData := NewTest("*/* 1","text/html","text/html","1")
     testData.AddHandler("*/*","1")
     testData.DoTests(t)
 
-    testData = NewTest("*/*","*/*","1")
+    testData = NewTest("*/* 2","*/*","*/*","1")
     testData.AddHandler("*/*","1")
     testData.DoTests(t)
 
-    testData = NewTest("","*/*","1")
+    testData = NewTest("*/* 3","","*/*","1")
     testData.AddHandler("*/*","1")
     testData.DoTests(t)
 }
 
 // Tests for two routers 1: "text/html", 2: "*/*"
 func TestTypeNegotiator3(t *testing.T) {
-    testData := NewTest("text/html","text/html","1")
+    testData := NewTest("text/html + */* 1","text/html","text/html","1")
     testData.AddHandler("text/html","1")
     testData.AddHandler("*/*","2")
     testData.DoTests(t)
 
-    testData = NewTest("text/html","text/html","2")
+    testData = NewTest("text/html + */* 2","text/html","text/html","2")
     testData.AddHandler("text/plain","1")
     testData.AddHandler("*/*","2")
     testData.DoTests(t)
 
-    testData = NewTest("text/*","text/html","1")
+    testData = NewTest("text/html + */* 3","text/*","text/*","2")
     testData.AddHandler("text/html","1")
     testData.AddHandler("*/*","2")
     testData.DoTests(t)
@@ -138,19 +140,37 @@ func TestTypeNegotiator3(t *testing.T) {
 
 // Two routers: 1: text/html, 2: text/plain
 func TestTypeNegotiator4(t *testing.T) {
-    testData := NewTest("text/html","text/html","1")
+    testData := NewTest("text/html + text/plain 1","text/html","text/html","1")
     testData.AddHandler("text/html","1")
     testData.AddHandler("text/plain","2")
     testData.DoTests(t)
 
-    testData = NewTest("text/plain","text/plain","1")
-    testData.AddHandler("text/plain","1")
-    testData.AddHandler("text/html","2")
+    testData = NewTest("text/html + text/plain 2","text/plain","text/plain","2")
+    testData.AddHandler("text/html","1")
+    testData.AddHandler("text/plain","2")
     testData.DoTests(t)
 
-    testData = NewTest("text/*","text/html","1")
+    testData = NewTest("text/html + text/plain 3","text/*","text/*","1")
     testData.AddHandler("text/*","1")
     testData.AddHandler("text/plain","2")
     testData.DoTests(t)
+}
+
+// Three routers: 1: */*, 2: text/html, 3: text/plain
+func TestTypeNegotiator5(t *testing.T) {
+    testData := NewTest("*/* + text/html + text/plain 1", "text/html","text/html","2")
+    testData.AddHandler("*/*","1")
+    testData.AddHandler("text/html","2")
+    testData.AddHandler("text/plain","3")
+
+    testData = NewTest("*/* + text/html + text/plain 2", "text/html","text/foo","1")
+    testData.AddHandler("*/*","1")
+    testData.AddHandler("text/html","2")
+    testData.AddHandler("text/plain","3")
+
+    testData = NewTest("*/* + text/html + text/plain 3", "text/*","text/*","2")
+    testData.AddHandler("*/*","1")
+    testData.AddHandler("text/html","2")
+    testData.AddHandler("text/plain","3")
 
 }
